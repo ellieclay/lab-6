8390dcf73f3712b224fa3aa69306690e82006ef0
Elizabeth Clayman <elizabethclayman@Elizabeths-MacBook-Pro-2.local>
2023-03-29 20:55:37 -0400

    added main function

--- main.py
+++ main.py
@@ -9,6 +9,33 @@ def encode(s):
             result += str(abs((int(i)+3) - 10))
     return result
 
-t = "12345555"
 
-print(encode(t))
+
+def main():
+    running = True
+    pw = "None"
+    opw = ''
+    while running:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit\n")
+
+        sel = input("Please enter an option: ")
+        if sel == '1':
+            opw = input("Please enter your password to encode: ")
+            pw = encode(opw)
+
+        elif sel == '2':
+            if pw != 'None':
+                print(f"The encoded password is {pw}, and the original password is {opw}")
+        elif sel == '3':
+            running = False
+
+
+main()
+
+
+
+
